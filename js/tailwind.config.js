4a643c529f2f4dbb40cfbbd7d494171cf1a91fba
nazsakib <sakibnshakhawat@gmail.com>
2022-10-02 20:50:12 +0600

    v1

---
+++ js/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            primary: "#243c5a",
+            secondary: {
+                100: "#243c5a",
+            },
+        },
+    },
+    plugins: [],
+};
